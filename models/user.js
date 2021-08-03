61b45376c055571807a72bae61fa002170537105
Deeshan Sharma <deeshan.sharma@ashmar.in>
2021-08-03 12:24:54 +0530

    ✨ feat: implement authorization with roles

--- models/user.js
+++ models/user.js
@@ -27,6 +27,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: '',
   },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['admin', 'user', 'publisher'],
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);

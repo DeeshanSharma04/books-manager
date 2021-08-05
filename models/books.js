7839179235cb44d2c2848999ca3a51fac8b37050
Deeshan Sharma <deeshan.sharma@ashmar.in>
2021-08-05 09:56:05 +0530

    ✨ feat: add validations in models

--- models/books.js
+++ models/books.js
@@ -4,18 +4,25 @@ const bookSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    minlength: 3,
+    maxlength: 50,
   },
   page: {
     type: Number,
     required: true,
+    trim: true,
+    max: 5000,
   },
   authorID: {
     type: mongoose.Schema.ObjectId,
     required: true,
+    trim: true,
   },
   price: {
     type: Number,
     required: true,
+    trim: true,
+    max: 9000,
   },
   token: {
     type: String,
@@ -24,6 +31,7 @@ const bookSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
     enum: [true, false],
+    trim: true,
   },
 });
 
--- models/user.js
+++ models/user.js
@@ -4,16 +4,21 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
     minlength: 3,
-    maxlength: 20
+    maxlength: 20,
   },
   age: {
     type: Number,
     required: true,
+    min: 10,
+    max: 80,
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    maxlength: 50,
     validate: (email) => {
       return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
         email
@@ -33,6 +38,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'user',
     enum: ['admin', 'user', 'publisher'],
+    trim: true,
   },
 });
 
